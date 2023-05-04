c54f2da965e2e3a97289b43f049655ea28c9eb70
riet <adamriet@gmail.com>
2023-05-04 11:49:49 -0600

    created fs.read/write in server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const public = require('./routes/public');
+const fs = require('fs');
 const PORT = process.env.PORT || 3001;
 
 
@@ -14,11 +14,45 @@ app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.get('/', (req, res) => 
+app.get('/notes', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile('db.json', 'utf-8', (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    });
+});
+
+
+
+
+app.post('/api/notes', (req, res) => { 
+    fs.readFile('db.json', 'utf-8', (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        const newNote = req.body;
+        newNote.id = Date.now();
+        notes.push(newNote);
+
+        fs.writeFile('db.json', JSON.stringify(notes), (err) => {
+            if (err) throw err;
+            console.log(err);
+            res.json(newNote);
+        });
+    });
+});
+
+
+
+
+
+
 
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
+
+
