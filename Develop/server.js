98a0ab8da61c82c985ccf2a466f01907bbf3e3c9
riet <adamriet@gmail.com>
2023-05-05 13:17:41 -0600

    altered app use static file

--- Develop/server.js
+++ Develop/server.js
@@ -13,7 +13,7 @@
 //Middleware setup
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
-    app.use(express.static('public'));
+    app.use(express.static(path.join(__dirname, 'public')));
 
 //Routes
 
